57dcd04c0e56629cdbd81b6f2cb10e4aa8a9dd0b
codethi <docencia.thiago@gmail.com>
2024-07-17 16:46:25 -0300

    feat: prisma and user module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserModule } from './modules/users/user.modules';
+import { PrismaModule } from './modules/prisma/prisma.module';
 
 @Module({
-  imports: [],
+  imports: [PrismaModule, UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
